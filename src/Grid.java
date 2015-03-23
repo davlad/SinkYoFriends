1444b3a91c607b7cb5e806e9729adc17add1eb1b
Daniel V. Gurevich <daniel@fuzzy.den.paulidav.org>
2015-03-23 13:36:51 -0700

    Added files

---
+++ src/Grid.java
@@ -0,0 +1,33 @@
+
+public class Grid {
+	
+	private int[][] grid;
+	
+	public Grid(int x, int y) {
+		grid = new int[x][y];
+	}
+	
+	public int[][] getGrid() {
+		return grid;
+	}
+	
+	public int shoot(int x, char y) {
+		if ()
+		
+		
+		return 1;
+	}
+	
+	public int charToInt(char y) {
+		
+		return i;
+	}
+	
+	public boolean inBounds(int x, int y) {
+		if () {
+			
+		}
+		return false;
+	}
+	
+}
