bfa2dcab21c813b98d30a93de63e96639a99a3d4
Daniel V. Gurevich <daniel@fuzzy.den.paulidav.org>
2015-03-24 13:55:49 -0700

    Add message stawberries

--- src/Boat.java
+++ src/Boat.java
@@ -1,4 +1,20 @@
 
 public class Boat {
 
+	private int length;
+	private int xPos;
+	private int yPos;
+	private int dir;
+	
+	public Boat(int l, int x, int y, int d) {
+		checkParams(l, x, y, d);
+	}
+
+	private void checkParams(int l, int x, int y, int d) {
+		if (l < 2 || l > 5) {
+			System.out.println(Can't crate boat: illegal length);
+		} else if 
+		
+	}
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	}
 	
-	public void battleship {
+	public void battleship() {
 		
 	}
 
